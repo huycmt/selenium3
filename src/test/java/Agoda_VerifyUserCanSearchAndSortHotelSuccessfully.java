abe5a47470837c5ae34f36dac94cffdc4cd558c2
Nguyen Duc Quang Huy <quanghuy.nguyen@loggiear.com>
2023-07-21 01:00:32 +0700

    support parallel

--- src/test/java/Agoda_VerifyUserCanSearchAndSortHotelSuccessfully.java
+++ src/test/java/Agoda_VerifyUserCanSearchAndSortHotelSuccessfully.java
@@ -1,10 +1,8 @@
 import base.TestBase;
 
-import com.codeborne.selenide.testng.SoftAsserts;
 import org.example.page.agoda.HomePage;
 import org.example.page.agoda.ResultPage;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import java.time.DayOfWeek;
@@ -13,7 +11,6 @@ import java.time.temporal.TemporalAdjusters;
 
 import static org.example.driver.DriverManager.driver;
 
-//@Listeners({SoftAsserts.class})
 public class Agoda_VerifyUserCanSearchAndSortHotelSuccessfully extends TestBase {
 
     @BeforeMethod
@@ -44,6 +41,4 @@ public class Agoda_VerifyUserCanSearchAndSortHotelSuccessfully extends TestBase 
     ResultPage resultPage = new ResultPage();
     String place;
     LocalDate threeDaysFromNextFriday;
-
-
 }
